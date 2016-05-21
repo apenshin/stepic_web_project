94277300f159c3b1b762d1c4f492412023bc65af
apenshin <apenshin@gmail.com>
2016-05-21 15:43:01 +0300

    Правки формы

--- init.sh
+++ init.sh
@@ -8,12 +8,6 @@ sudo rm /etc/gunicorn.d/ask
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart 
-#sudo /etc/init.d/mysql restart
 
-# ������������� ����
-mysql -uroot -e "DROP DATABASE ASK"
-mysql -uroot -e "CREATE DATABASE ASK"
-mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
 
 python /home/box/web/ask/manage.py syncdb
