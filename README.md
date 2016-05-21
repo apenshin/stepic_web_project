0608557c5e335c66c4b34aa1a076ea7983dc0a23
apenshin <apenshin@gmail.com>
2016-05-21 13:39:55 +0300

    Initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+#Курс WEB технологии
+Clone project:
+
+    git clone https://github.com/apenshin/stepic_web_project.git /home/box/web
+
+Start project:
+
+    bash /home/box/web/init.sh
---
+++ init.sh
@@ -0,0 +1,19 @@
+# ������������� �������
+sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo rm /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/ask
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart 
+#sudo /etc/init.d/mysql restart
+
+# ������������� ����
+mysql -uroot -e "DROP DATABASE ASK"
+mysql -uroot -e "CREATE DATABASE ASK"
+mysql -uroot -e "CREATE USER 'sa'@'localhost' IDENTIFIED BY 'sa'"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ASK.* TO 'sa'@'localhost'"
+
+python /home/box/web/ask/manage.py syncdb
